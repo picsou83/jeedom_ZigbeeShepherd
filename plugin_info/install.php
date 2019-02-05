65f7d508f0086734c08123f58a7f856b3b38384a
picsou83 <34648108+picsou83@users.noreply.github.com>
2019-02-05 23:13:16 +0100

    Add files via upload

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -18,8 +18,8 @@
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
-function espeasy_update() {
-    espeasy::deamon_stop();
+function zigbee_update() {
+    zigbee::deamon_stop();
     $sensor_path = dirname(__FILE__) . '/../resources';
     exec('cd ' . $sensor_path . '; rm -rf node_modules');
 }
